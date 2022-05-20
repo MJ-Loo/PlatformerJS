ba759bda60125b77a95f4e3b0c66a0f43db8e076
jackalope331 <ba007895@gmail.com>
2022-05-20 22:44:58 +0200

    Level 2 model placements
    Preliminary placements for the 2nd level. Structure and beams placed.
    No lighting, shadows or textures yet.
    
    Updated win condition to end of level 2

--- js/objects/Platforms.js
+++ js/objects/Platforms.js
@@ -11,16 +11,46 @@ export class Platforms{
     }
     
     createStatic(){
-      this.StaticPlatforms([7,1,7],[-10,2,25]);
-      this.StaticPlatforms([4,1,4],[-20,-1,35]);
-      this.StaticPlatforms([5,1,5],[7,0,35]);
-      this.StaticPlatforms([6,1,6],[-4,-3,40]);
-      this.StaticPlatforms([5,1,5],[-12,0,44]);
-      this.StaticPlatforms([5,1,5],[7,1,49]);
-      this.StaticPlatforms([5,1,5],[-6,2,55]);
-      this.StaticPlatforms([5,1,5],[6,-4,60]);
-      this.StaticPlatforms([5,1,5],[-5,-4,70]);
-      this.StaticPlatforms([5,1,5],[10,-4,99]);
+      this.StaticPlatforms([7,1,7],[-7,2,20]);
+      this.StaticPlatforms([4,1,4],[5,-1,16]);
+      this.StaticPlatforms([5,1,5],[5,0,24]);
+      this.StaticPlatforms([6,1,6],[-2,-3,32]);
+      this.StaticPlatforms([5,1,5],[-12,0,35]);
+      this.StaticPlatforms([5,1,5],[4,1,39]);
+      this.StaticPlatforms([5,1,5],[-6,2,49]);
+      this.StaticPlatforms([5,1,5],[2,-4,56]);
+      this.StaticPlatforms([5,1,5],[5,-4,65]);
+      
+      
+      //Level 2 start
+      //this.StaticPlatforms([16, 1, 500], [30, -1, 100]); //debug platform too lazy to do level 1
+      //this.StaticPlatforms([1, 1, 50], [10, -4, 50]); //debug platform
+
+      this.StaticPlatforms([9, 1, 5], [12, -1, 100]);
+      this.StaticPlatforms([11, 8, 9.5], [12, 2, 110.25]); //front wall
+      this.StaticPlatforms([1, 14, 20], [7, 0, 105]);//right side wall
+      this.StaticPlatforms([1, 14, 20], [17, 0, 105]);//left side wall
+      this.StaticPlatforms([11, 21, 1], [12, -12, 98]); //back wall
+      this.StaticPlatforms([11, 1, 20], [12, -7, 108]); //level floor
+      this.StaticPlatforms([11, 1, 100], [12, -2, 156]); //ceiling
+      this.StaticPlatforms([1, 20, 111], [17, -12, 154]); //corridor (left)
+      this.StaticPlatforms([1, 20, 111], [7, -12, 154]); //corridor (right)
+      this.StaticPlatforms([11, 1, 111], [12, -22, 154]); //floor (void)
+      this.StaticPlatforms([11, 20.5, 1], [12, -12.25, 210]); //end wall
+
+      this.StaticPlatforms([0.3, 0.5, 10], [13, -7, 120]); //first platform
+      this.StaticPlatforms([2, 0.5, 0.3], [12, -7, 125]);
+      this.StaticPlatforms([0.3, 0.5, 3], [11, -7, 127]);
+      this.StaticPlatforms([1, 0.5, 0.1], [11, -7, 130]);
+      this.StaticPlatforms([0.2, 0.4, 17], [14, -7, 137]);
+      this.StaticPlatforms([5, 0.4, 0.2], [12, -6, 135]);
+      this.StaticPlatforms([5, 0.4, 0.2], [12, -6, 145]);
+      this.StaticPlatforms([3, 0.2, 0.1], [14, -7, 150]);
+      this.StaticPlatforms([0.1, 0.2, 20], [11, -6, 155]);
+      this.StaticPlatforms([0.1, 0.2, 14], [15, -7, 158]);
+      this.StaticPlatforms([8, 0.2, 0.1], [12, -6, 165]);
+      this.StaticPlatforms([0.01, 0.01, 20], [12, -6, 175]);
+      this.StaticPlatforms([5, 1, 25], [12, -6, 196]);
     }
 
     StaticPlatforms(scale,position){ 
